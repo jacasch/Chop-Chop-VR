ae0fda4c8de4a3fa58142ea945f92300ea586d8a
jacasch <junk.u.spam@gmail.com>
2018-03-05 21:15:03 +0100

    Adde Steam VR Asset and Implementet log splitting

---
+++ Assets/Scripts/Axe.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class Axe : MonoBehaviour {
+    //Velocity stuff
+    private Vector3 lastFramePos;
+    public Vector3 velocity;
+    public float tiltAngle;
+
+    #region initialize components
+    Collider col;
+    Rigidbody rb;
+    #endregion
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        UpdatVelocity();
+        UpdateTiltAngle();
+	}
+
+    void UpdatVelocity() {
+        Vector3 frameMovementDistance = transform.position - lastFramePos;
+        velocity = frameMovementDistance / Time.deltaTime;
+        lastFramePos = transform.position;
+    }
+
+    void UpdateTiltAngle() {
+        //tweak this according to model
+        Vector3 bladeNormal = transform.parent.forward;
+        Vector3 projectedVelocityY = Vector3.ProjectOnPlane(velocity, bladeNormal);
+        float tiltOnBladeAxis = Vector3.Angle(velocity, projectedVelocityY); //calculates the rotation along the blade edge
+
+        float tiltInAllDirections = Vector3.Angle(velocity, transform.parent.right); //calculates the tilt based on the pointing direction of the blade
+
+        tiltAngle = tiltInAllDirections;
+    }
+}
---
+++ Assets/Scripts/WoodLog.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WoodLog : MonoBehaviour {
+
+    public float cutThreshold;
+    public float splitResitance;
+
+    public GameObject logSplit;
+
+    private bool beingCut = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public virtual void ChopHit() { }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        print("collision");
+        if (collider.gameObject.layer == LayerMask.NameToLayer("Blade")) {
+            //collision with axe Blade
+            float tiltAngle = collider.gameObject.GetComponent<Axe>().tiltAngle;
+            Vector3 velocity = collider.gameObject.GetComponent<Axe>().velocity;
+
+            //print(tiltAngle);
+
+            CalculateHitImpact(collider.transform.parent, velocity, tiltAngle);
+        }
+    }
+
+    private void CalculateHitImpact(Transform axe, Vector3 bladeVelocity, float bladeTiltAngle) {
+        float impactAngle = Vector3.Angle(Vector3.down, bladeVelocity.normalized);
+
+        if (bladeTiltAngle <= 30) {
+            //blade is is not tiltet off os there could actually be a cut
+            if (impactAngle <= 30) {
+                //we are hitting the log from the top and not the sides or anything
+                float bladeVelocityMadnitude = bladeVelocity.magnitude;
+                print(bladeVelocityMadnitude);
+                if (bladeVelocityMadnitude <= cutThreshold) {
+                    //we are not Fast Enough
+                    //HIT THE LOG
+                } else
+                {
+                    //at least fast enough to hit the log
+                    if (bladeVelocityMadnitude <= splitResitance)
+                    {
+                        //we have enough impact to cut into the wood, but not enough to split it
+                        //CUT INTO Log
+                    } else
+                    {
+                        //Impact is enough to split the log in half
+                        SplitLog(axe);
+                    }
+                }
+            }
+        }
+    }
+
+    private void HitLog()
+    {
+        ////diable physics components of log
+        //instantiate log rigidbody in position of log
+        //apply force to instance
+        //destroy gameopbject
+    }
+    private void CutLog(Transform axe, Vector3 impactVelocity)
+    {
+        beingCut = true;
+        //set axe as parent
+        transform.parent = axe.transform;
+        //move log up the blade according to impact strength
+        Vector3 localPos = transform.localPosition;
+        localPos -= impactVelocity;
+        splitResitance -= impactVelocity.magnitude;
+        transform.localPosition = localPos;
+
+    }
+    private void SplitLog(Transform axe)
+    {
+        /*//diable physics components of log
+        gameObject.GetComponent<Collider>().enabled = false;*/
+        //instanitate 2 logsplits in position of log
+        GameObject logInstance1 = Instantiate(logSplit, transform.position, transform.rotation, null);
+        GameObject logInstance2 = Instantiate(logSplit, transform.position, transform.rotation, null);
+        //rotate to blade
+        logInstance1.transform.Rotate(0, 0, 90);
+        logInstance2.transform.Rotate(0, 0, -90);
+        //apply force to splits
+        logInstance1.GetComponent<Rigidbody>().AddForceAtPosition(logInstance1.transform.forward * 20, logInstance1.transform.position + new Vector3(0, 0, 1));
+        logInstance2.GetComponent<Rigidbody>().AddForceAtPosition(logInstance2.transform.forward * -20, logInstance2.transform.position + new Vector3(0, 0, 1));
+        //slogInstance.GetComponent<Rigidbody>().AddForce(Vector3.up * 100);
+        //destroy gameopbject
+        Destroy(gameObject);
+    }
+}
