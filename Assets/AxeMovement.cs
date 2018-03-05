f970eaf40f0024b81ba824614a663add01239b95
Jan Carlo Schneider <junk.u.spam@gmail.com>
2018-03-05 16:34:15 +0100

    Ïnitial Sumbmit from offline to git

---
+++ Assets/AxeMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxeMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*Vector3 pos = transform.position;
+        pos.x = Mathf.Sin(Time.time) * 10;
+        transform.position = pos;*/
+
+        Vector3 rot = transform.rotation.eulerAngles;
+        rot.x += Time.deltaTime * 10;
+        rot.x %= 360;
+        transform.rotation = Quaternion.Euler(rot);
+    }
+}
---
+++ Assets/RegularWoodLog.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RegularWoodLog : WoodLog {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
